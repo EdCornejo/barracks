1e3bec0bd29b65705e1eb80a5b4e2d200ee4e839
Ed Cornejo <zed.hyde@icloud.com>
2021-10-26 11:53:36 -0500

    feat: add user model

--- backend/barracks/user/models.py
+++ backend/barracks/user/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password, **kwargs):
+        if password is None:
+            raise TypeError('Password is required')
+        if username is None:
+            raise TypeError('Username is required')
+        if email is None:
+            raise TypeError('Email is required')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True,  null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
