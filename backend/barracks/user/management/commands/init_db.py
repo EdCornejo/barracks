d1ee06352333a896ad7a41c5e68e19cc03187d76
Ed Cornejo <zed.hyde@icloud.com>
2021-10-27 12:58:11 -0500

    feat: improve init db

--- backend/barracks/user/management/commands/init_db.py
+++ backend/barracks/user/management/commands/init_db.py
@@ -7,4 +7,6 @@ class Command(BaseCommand):
     help = 'Init db'
 
     def handle(self, *args, **kwargs):
+        User.objects.all().delete()
         User.objects.create_superuser(username='admin', email='admin@ejemplo.poc', password='admin123')
+        User.objects.create_user(username='demo', email='demo@mail.poc', password='demo123')
