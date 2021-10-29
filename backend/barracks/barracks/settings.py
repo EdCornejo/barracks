8d609967b85d530f540405b03e59ee713bc6884a
Ed Cornejo <zed.hyde@icloud.com>
2021-10-29 02:21:44 -0500

    fix: cors

--- backend/barracks/barracks/settings.py
+++ backend/barracks/barracks/settings.py
@@ -182,3 +182,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5000",
     "http://54.167.177.47:5000",
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:5000',
+    'http://127.0.0.1:5000',
+    'http://54.167.177.47:5000',
+]
